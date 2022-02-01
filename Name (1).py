c73950232443b710320053b71008130810b55a3c
raimondskrauklis <63188387+raimondskrauklis@users.noreply.github.com>
2022-02-01 09:56:59 +0100

    Add files via upload

---
+++ Name (1).py
@@ -0,0 +1,12 @@
+print("Raimonds")
+
+dienas_gada = 366
+sekundes_minute = 60
+stundas_diennakti = 24
+sekundes_stunda = sekundes_minute * sekundes_minute
+sekundes_diennakti = sekundes_stunda * stundas_diennakti
+sekundes_gada = dienas_gada * sekundes_diennakti
+print(f"Gadā ir {sekundes_gada} sekundes.")
+
+a = "Raimonds" * 7
+print(a) #vārds izdrukāsies septiņas reizes
