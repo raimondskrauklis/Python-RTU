7344d5cc56e193fe49b69f699af31153ed95d981
raimondskrauklis <63188387+raimondskrauklis@users.noreply.github.com>
2022-02-01 10:34:17 +0100

    Add files via upload

---
+++ C uz F (4).py
@@ -0,0 +1,6 @@
+temp_celsius = input(f"Lūdzu, ievadiet temperatūru Celsija mērvienībā: ")
+temp_celsius = int(temp_celsius)
+a = 32
+a = int(a)
+temp_fahrenheit = a + temp_celsius*9/5
+print(f"Temperatûra Fahreinheit mērvienībā ir {temp_fahrenheit} grādi")
